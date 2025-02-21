186ced46ca937c9ab2cec541900a32c026968de8
Olívia Guimarães <rgolivia54@gmail.com>
2025-02-21 13:41:25 -0500

    Updated Repository Name

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'to-do-pwa-cache-v1';
 const FILES_TO_CACHE = [
-    '/Info5146Website/',
-    '/Info5146Website/index.html',
-    '/Info5146Website/style.css',
-    '/Info5146Website/app.js',
-    '/Info5146Website/manifest.json',
-    '/Info5146Website/icons/icon-128.png',
-    '/Info5146Website/icons/icon-512.png'
+    '/Info5146ToDoApp/',
+    '/Info5146ToDoApp/index.html',
+    '/Info5146ToDoApp/style.css',
+    '/Info5146ToDoApp/app.js',
+    '/Info5146ToDoApp/manifest.json',
+    '/Info5146ToDoApp/icons/icon-128.png',
+    '/Info5146ToDoApp/icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
